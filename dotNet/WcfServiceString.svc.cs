25253dda198bd93ad275a1fae047ad5cd15afde3
권대선 <dsun@hanmail.net>
2020-10-21 14:11:24 +0900

    신규

---
+++ dotNet/WcfServiceString.svc.cs
@@ -0,0 +1,14 @@
+﻿using Atomus.Diagnostics;
+using System;
+
+namespace Atomus.Service
+{
+    public class WcfServiceString : IServiceString
+    {
+        string IServiceString.Request(string data)
+        {
+            //return (new ServerStringAdapter() as IServiceString).Request(data);
+            return (this.CreateInstance("ServerAdapter") as IServiceString).Request(data);
+        }
+    }
+}
